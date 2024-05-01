4b664b4c109c0d1d7da245874de8e4a965894e8d
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-05-01 16:46:31 -0500

    install and init nativewind + prettier plugin sort

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
-import {View, Text} from 'react-native'
+import { View, Text } from "react-native";
 
 const Index = () => {
   return (
-      <View>
-        <Text>Hello World!!</Text>
-      </View>
-  )
-}
-export default Index
+    <View className="flex-1 justify-center bg-sky-300">
+      <Text className="text-center text-xl font-semibold text-white">
+        Hello World!
+      </Text>
+    </View>
+  );
+};
+export default Index;
